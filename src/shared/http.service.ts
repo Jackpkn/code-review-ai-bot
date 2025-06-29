5a4d9331eb3a3897a3f2b906a600da04bb658338
jackpk <pawankumarbairwa879@gmail.com>
2025-06-29 18:24:11 +0530

    feat(http): implement HttpService for handling HTTP requests

---
+++ src/shared/http.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { HttpService as NestHttpService } from '@nestjs/axios';
+import { Observable } from 'rxjs';
+import { AxiosResponse } from 'axios';
+@Injectable()
+export class HttpService {
+  constructor(private readonly httpService: NestHttpService) {}
+  get<T = any>(url: string, config?: any): Observable<AxiosResponse<T>> {
+    return this.httpService.get(url, config);
+  }
+  POST<T = any>(
+    url: string,
+    data?: any,
+    config?: any,
+  ): Observable<AxiosResponse<T>> {
+    return this.httpService.post(url, data, config);
+  }
+  put<T = any>(
+    url: string,
+    data?: any,
+    config?: any,
+  ): Observable<AxiosResponse<T>> {
+    return this.httpService.put(url, data, config);
+  }
+  delete<T = any>(url: string, config?: any): Observable<AxiosResponse<T>> {
+    return this.httpService.delete(url, config);
+  }
+}
