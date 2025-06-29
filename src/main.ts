cad638eb93d17bcc53de07724515a52d231f2198
jackpk <pawankumarbairwa879@gmail.com>
2025-06-29 18:08:13 +0530

    feat(main): add CORS support and global prefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { log } from 'console';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
+  });
+  // ENABLE CORS FOR WEBHOOK
+  app.enableCors();
+  // global prefix
+  app.setGlobalPrefix('api');
+  const port = process.env.PORT ?? 5000;
+  await app.listen(port);
+  log(`PR Review BOT is running on http://localhost:${port}`);
 }
 bootstrap();
