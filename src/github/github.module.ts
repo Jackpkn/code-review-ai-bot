699d4107cd5ccdb7426b629f421f73ad30182c92
jackpk <pawankumarbairwa879@gmail.com>
2025-06-29 19:58:53 +0530

    feat(github): add HttpService and import HttpModule in GithubModule

--- src/github/github.module.ts
+++ src/github/github.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GithubService } from './github.service';
+import { HttpModule } from '@nestjs/axios/dist/http.module';
+import { HttpService } from 'src/shared/http.service';
 
 @Module({
-  providers: [GithubService]
+  providers: [GithubService, HttpService],
+  imports: [HttpModule],
 })
 export class GithubModule {}
