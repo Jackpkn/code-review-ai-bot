cbe9bf3a0b66c3132f5235d0a9027fb58e880927
jackpk <pawankumarbairwa879@gmail.com>
2025-06-29 17:50:16 +0530

    feat(logger): implement LoggerService using pino for structured logging

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
-
+import {
+  Injectable,
+  LogLevel,
+  LoggerService as NestLoggerService,
+} from '@nestjs/common';
+import pino from 'pino';
 @Injectable()
-export class LoggerService {}
+export class LoggerService implements NestLoggerService {
+  private readonly logger: pino.Logger;
+  constructor() {
+    this.logger = pino({
+      level: process.env.LOG_LEVEL || 'info',
+      transport: {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'SYS:standard',
+          ignore: 'pid,hostname',
+        },
+      },
+    });
+  }
+  log(message: any, context?: string) {
+    this.logger.info({ context }, message);
+  }
+
+  error(message: any, trace?: string, context?: string) {
+    this.logger.error({ context, trace }, message);
+  }
+
+  warn(message: any, context?: string) {
+    this.logger.warn({ context }, message);
+  }
+
+  debug(message: any, context?: string) {
+    this.logger.debug({ context }, message);
+  }
+
+  verbose(message: any, context?: string) {
+    this.logger.trace({ context }, message);
+  }
+  fatal?(message: any, ...optionalParams: any[]) {
+    this.logger.fatal({ message }, ...optionalParams);
+  }
+  // TODO: IMPLEMENT LATER ON
+  setLogLevels?(levels: LogLevel[]) {
+    throw new Error('Method not implemented.');
+  }
+}
