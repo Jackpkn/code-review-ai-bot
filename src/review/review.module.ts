cac0ddbdca8bdcf1fc217e0b6f7d6a269d7edd45
jackpk <pawankumarbairwa879@gmail.com>
2025-06-29 21:19:32 +0530

    feat(review): add SharedModule import to ReviewModule for improved modularity

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ReviewService } from './review.service';
+import { SharedModule } from '../shared/shared.module';
 
 @Module({
-  providers: [ReviewService]
+  imports: [SharedModule],
+  providers: [ReviewService],
+  exports: [ReviewService],
 })
-export class ReviewModule {}
+export class ReviewModule { }
